65a97541851abc085a005561ffbdf5bfb139371c
ECrisantosMontes <156939016+ECrisantosMontes@users.noreply.github.com>
2024-01-31 22:07:28 +0000

    Updated lab 2

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 function setup(){
   createCanvas(600, 400);
+  strokeWeight(5);
 }
 
 function draw(){
-  background(200);
+  background(210, 240, 255);
   // add your drawing code here ...
-  fill(640, 900, 150)
-  ellipse(150,50,50)
+  stroke(100, 10, 160)
+  fill(130, 0, 200)
+  ellipse(450,300,40)
+  ellipse(431, 271, 40)
+  ellipse(470, 270, 40)
+  ellipse(415, 245, 40)
+  ellipse(485, 245, 40)
+  ellipse(450, 246, 40)
+  ellipse(422, 217, 38)
+  ellipse(448, 212, 40)
+  ellipse(479, 217, 40)
+  noFill()
+  stroke(100, 200, 100)
+  arc(400, 190, 60, 60, 390, 60)
+  stroke(250, 240, 120)
+  fill(250, 250, 200)
+  triangle(60, 200, 20, 80, 100, 80)
+  line(60, 80, 81, 127)
+  line(40, 80, 73, 160)
+  line(49, 175, 80, 80)
+  line(40, 142, 60, 80)
+  line(30, 110, 40, 80)
+  line(90, 110, 80, 80)
+  fill(255, 220, 255)
+  stroke(255, 200, 255)
+  arc(60, 50, 80, 80, 380, 176)
+  arc(96, 74, 40, 40, 362, 84)
+  arc(63, 74, 40, 40, 377, 47)
+  arc(27, 74, 40, 40, 76, 376);
+  stroke(255, 220, 255)
+  ellipse(84, 44, 15)
+  ellipse(70, 60, 10)
 }
